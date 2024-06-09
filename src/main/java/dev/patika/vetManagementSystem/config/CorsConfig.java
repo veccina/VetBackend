42341c17d5222bfac8e3f635115d35812b74fd33
Baris Can Berki <av.bariscanberki@gmail.com>
2024-06-09 03:23:43 +0300

    Config

---
+++ src/main/java/dev/patika/vetManagementSystem/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package dev.patika.vetManagementSystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://6664f1f6b7dd974d1416fc72--sprightly-melomakarona-5bc36f.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
